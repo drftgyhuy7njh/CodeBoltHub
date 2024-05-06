d740e1943d758ab55f3a28b432b5d5605469ee6a
drftgyhuy7njh <gs1antip682@hotmail.com>
2024-05-07 03:51:00 +0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const count = Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - 97]++;
+    count[t.charCodeAt(i) - 97]--;
+  }
+  return count.every((c) => c === 0);
+}
