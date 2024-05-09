177c578ce3d95acee381059e5a5bae008c10abc6
drftgyhuy7njh <gs1antip682@hotmail.com>
2024-05-10 03:51:58 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function largestNumber(nums) {
-  if (nums.every((num) => num === 0)) return "0";
-  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
+function longestPalindrome(s) {
+  const map = new Map();
+  let hasOdd = false;
+  let result = 0;
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  for (const count of map.values()) {
+    if (count % 2 === 0) result += count;
+    else {
+      result += count - 1;
+      hasOdd = true;
+    }
+  }
+  return hasOdd ? result + 1 : result;
 }
