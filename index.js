dac691e102d72921aba9ddaf83cd14f546994b64
drftgyhuy7njh <gs1antip682@hotmail.com>
2024-05-11 02:44:34 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-function longestPalindrome(s) {
-  const map = new Map();
-  let hasOdd = false;
-  let result = 0;
-  for (const char of s) {
-    map.set(char, (map.get(char) || 0) + 1);
-  }
-  for (const count of map.values()) {
-    if (count % 2 === 0) result += count;
-    else {
-      result += count - 1;
-      hasOdd = true;
+function multiply(num1, num2) {
+  const m = num1.length;
+  const n = num2.length;
+  const pos = new Array(m + n).fill(0);
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      const mul = (num1[i] - "0") * (num2[j] - "0");
+      const p1 = i + j;
+      const p2 = i + j + 1;
+      const sum = mul + pos[p2];
+      pos[p1] += Math.floor(sum / 10);
+      pos[p2] = sum % 10;
     }
   }
-  return hasOdd ? result + 1 : result;
+  let result = "";
+  for (const p of pos) {
+    if (!(result.length === 0 && p === 0)) result += p;
+  }
+  return result.length === 0 ? "0" : result;
 }
